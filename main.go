6506b9284a510fd21159e47d7247d995f5d6c166
fkimani <mainafk@gmail.com>
2020-12-30 03:30:23 -0500

    process form template variables

--- main.go
+++ main.go
@@ -2,14 +2,48 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<html><head></head><body><h1>My fantastic webpage</h1></body></html>")
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/test", handlerFunc)
+	http.HandleFunc("/", index)
+	http.HandleFunc("/process", processor)
 	http.ListenAndServe(":3000", nil)
+}
+
+//example borrowed from prof mcleod
+func index(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+}
+
+func processor(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	fname := r.FormValue("firster")
+	lname := r.FormValue("laster")
+
+	d := struct {
+		First string
+		Last  string
+	}{
+		First: fname,
+		Last:  lname,
+	}
+
+	tpl.ExecuteTemplate(w, "processor.gohtml", d)
 }
